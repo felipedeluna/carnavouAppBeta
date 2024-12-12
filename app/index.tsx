6dad6f6c567e3b9fe8619308541dc7d76e8bf619
Dimittri Souza <dimittrisouza@gmail.com>
2024-12-11 21:32:33 -0300

    Criação da tela de login

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import DownNavBar from "../components/downNavBar"
+import Login from "../components/pages/login"
 import {SafeAreaProvider} from "react-native-safe-area-context"
-import { useFonts, Merriweather_400Regular, Merriweather_700Bold } from '@expo-google-fonts/merriweather';
-import { ActivityIndicator, MD2Colors } from 'react-native-paper';
+
 
 export default function App() {
 
   return (
         <SafeAreaProvider>
             <View style={{flex:1}}>
-                <DownNavBar />
+                <Login></Login>
             </View>
         </SafeAreaProvider>  
     );
